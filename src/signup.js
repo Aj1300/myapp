63e6c07ee358d7be477511f4d693158698f2138d
Aj1300 <antonyjees8@gmail.com>
2024-11-04 14:48:45 +0530

    register using state

--- src/signup.js
+++ src/signup.js
@@ -1,25 +1,38 @@
-import React from 'react'
-import './signup.css'
-function signup() {
-    
+import React, { useState } from 'react';
+import './signup.css';
+function Signup() {
+    const[formData,setFormData]= useState({
+      fname:"",
+      lname:"",
+      email:"",
+      password:""
+    })
+    const handleInputChange=(event)=>{
+      const {name,value}=event.target
+      setFormData((prevData)=>({...prevData,[name]:value}))
+    }
+    const handleSubmit=(event)=>{
+      event.preventDefault()
+      console.log(formData,"The data")
+    }
   return (
     <div className='regis'>
 
-      <form >
+      <form onSubmit={handleSubmit} >
         <h1>Registeration form</h1>
         <label>first name</label><br></br>
-        <input type='text'name='fname' id='fname'></input><br></br>
+        <input type='text'name='fname' id='fname' value={formData.fname} onChange={(event)=>handleInputChange(event)}></input><br></br>
         <label>last name</label><br></br>
-        <input type='text' name='lname' id='lname'></input><br></br>
+        <input type='text' name='lname' id='lname' value={formData.lname} onChange={(event)=>handleInputChange(event)}></input><br></br>
         <label>email</label><br></br>
-        <input type='email' name='email' id='email'></input><br></br>
+        <input type='email' name='email' id='email'value={formData.email} onChange={(event)=>handleInputChange(event)}></input><br></br>
         <label>password</label><br></br>
-        <input type='password' name='password' id='password'></input><br></br>
+        <input type='password' name='password' id='password' value={formData.password} onChange={(event)=>handleInputChange(event)}></input><br></br>
         <br></br>
-        <input type='submit'name='submit' value='register' onClick="register()"></input>
+        <input type='submit'name='submit' value='register' ></input>
       </form>
     </div>
   )
 }
 
-export default signup
+export default Signup
